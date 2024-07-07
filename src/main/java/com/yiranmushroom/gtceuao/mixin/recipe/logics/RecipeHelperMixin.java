8446d4ea629e7329d625d7ef346806db0c2de5bc
YiRanMushroom <yiranmushroom@gmail.com>
2024-07-07 22:46:05 +0800

    Attempted to fix generator issues.

---
+++ src/main/java/com/yiranmushroom/gtceuao/mixin/recipe/logics/RecipeHelperMixin.java
@@ -0,0 +1,9 @@
+package com.yiranmushroom.gtceuao.mixin.recipe.logics;
+
+import mezz.jei.forge.platform.RecipeHelper;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(RecipeHelper.class)
+public class RecipeHelperMixin {
+
+}
